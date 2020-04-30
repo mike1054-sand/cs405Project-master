fe4daba03d681897fd390f8ac1714a21b8e4702c
mike1054-sand <64096928+mike1054-sand@users.noreply.github.com>
2020-04-30 18:50:01 -0400

    Update DBEngine.java
    The block function and follow function will be added later. Used as placement holders for those functions until they are added.

--- src/main/java/edu/uky/cs405g/sample/database/DBEngine.java
+++ src/main/java/edu/uky/cs405g/sample/database/DBEngine.java
@@ -151,4 +151,44 @@ public class DBEngine {
         return userIdMap;
     } // getBDATE()
 
+	
+    public Map<String,String> sugUsers() {
+        Map<String,String> userIdMap = new HashMap<>();
+
+        PreparedStatement stmt = null;
+        try
+        {
+            Connection conn = ds.getConnection();
+            String queryString = null;
+            queryString = "SELECT * FROM Identity";
+            stmt = conn.prepareStatement(queryString);
+			// No parameters, so no binding needed.
+            ResultSet rs = stmt.executeQuery();
+			int i = 0;
+			while (rs.next()) {
+			    if (i < 4){
+			    String userId = Integer.toString(rs.getInt("idnum"));
+                            String userName = rs.getString("handle");
+				if (userName != block() && userName != follow()){
+				userIdMap.put(userId, userName);
+			         i++
+				}
+			}
+		}
+	}
+            rs.close();
+            stmt.close();
+            conn.close();
+  }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return userIdMap;
+}	
+	
+	
+	
+	
+	
 } // class DBEngine
